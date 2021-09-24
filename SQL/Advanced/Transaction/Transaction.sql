bd79b9ec0ba2cb76dbdfc008094dff3691f8ed10
Anastasiia <anastasiia_filchenko@yahoo.com>
2021-09-24 10:54:48 +0200

    Add files via upload

---
+++ SQL/Advanced/Transaction/Transaction.sql
@@ -0,0 +1,8 @@
+--DROP PROCEDURE TRANSACTION_ROSE;
+--CALL TRANSACTION_ROSE;  -- Caller query
+--SELECT * FROM BankAccounts;
+--SELECT * FROM ShoeShop;
+
+--CALL TRANSACTION_JAMES;  -- Caller query
+SELECT * FROM BankAccounts;
+SELECT * FROM ShoeShop;
